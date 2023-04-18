b7337342c032e494a25a1a171ac91fc182672667
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-19 01:10:14 +0530

    formatted code

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,9 +1,15 @@
 import { AppBar } from "@mui/material";
 
-export default function Header(){
-    return (
-        <AppBar sx={{backgroundColor:'transparent',boxShadow:0,padding:'10px'}}>
-            <img src="../logo.png" alt="logo" width='250px'/>
-        </AppBar>
-    )
+export default function Header() {
+  return (
+    <AppBar
+      sx={{
+        backgroundColor: "transparent",
+        boxShadow: 0,
+        padding: "10px"
+      }}
+    >
+      <img src="../logo.png" alt="logo" width="250px" />
+    </AppBar>
+  );
 }
