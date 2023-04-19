b3e39c5e82480e5676f701e7545eb04633bfbdaa
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-20 01:49:07 +0530

    disabled collapse and added multiline input for note content

--- client/src/NewNote.tsx
+++ client/src/NewNote.tsx
@@ -1,19 +1,41 @@
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import { useState } from "react";
+import Typography from "@mui/material/Typography";
+import Input from "@mui/material/Input";
 
 export default function NewNote() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <Accordion
       sx={{
-        width: "50%",
-        left: "25%",
-        right: "25%",
+        width: "40%",
+        left: "30%",
+        right: "30%",
         top: "20px",
       }}
+      onChange={() => {setIsExpanded(true)}}
+      expanded={isExpanded}
     >
-      <AccordionSummary>jnkjn</AccordionSummary>
-      <AccordionDetails>mjkn</AccordionDetails>
+      <AccordionSummary
+        sx={{
+          margin: "0"
+        }}
+        focusVisibleClassName="focus-visible"
+      >
+        <Typography
+          sx={{
+            color: "gray",
+            width: "100%"
+          }}>
+          {!isExpanded ? "Add New Note" : <Input fullWidth placeholder="Title" disableUnderline></Input>}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Input fullWidth placeholder="Add a note" disableUnderline multiline minRows={10} maxRows={15}></Input>
+      </AccordionDetails>
     </Accordion>
   );
 }
