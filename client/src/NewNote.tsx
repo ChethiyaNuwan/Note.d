907cd2ff146f7304698cac68bfe2f5807bfe3b68
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-20 03:14:58 +0530

    added buttons to accordion action

--- client/src/NewNote.tsx
+++ client/src/NewNote.tsx
@@ -4,9 +4,18 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Input from "@mui/material/Input";
+import { AccordionActions, Button, Stack } from "@mui/material";
 
 export default function NewNote() {
   const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleCancel = () => {
+    setIsExpanded(false);
+  };
+
+  const handleSave = () => {
+    setIsExpanded(false);
+  };
 
   return (
     <Accordion
@@ -16,26 +25,63 @@ export default function NewNote() {
         right: "30%",
         top: "20px",
       }}
-      onChange={() => {setIsExpanded(true)}}
+      onChange={() => {
+        setIsExpanded(true);
+      }}
       expanded={isExpanded}
     >
       <AccordionSummary
         sx={{
-          margin: "0"
+          margin: "0",
         }}
-        focusVisibleClassName="focus-visible"
       >
         <Typography
           sx={{
             color: "gray",
-            width: "100%"
-          }}>
-          {!isExpanded ? "Add New Note" : <Input fullWidth placeholder="Title" disableUnderline></Input>}
+            width: "100%",
+          }}
+        >
+          {!isExpanded ? (
+            "Add New Note"
+          ) : (
+            <Input fullWidth placeholder="Title" disableUnderline></Input>
+          )}
         </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Input fullWidth placeholder="Add a note" disableUnderline multiline minRows={10} maxRows={15}></Input>
+        <Input
+          fullWidth
+          placeholder="Add a note"
+          disableUnderline
+          multiline
+          minRows={10}
+          maxRows={10}
+        ></Input>
       </AccordionDetails>
+      <AccordionActions>
+        <Stack spacing={1} direction="row">
+          <Button
+            sx={{
+              backgroundColor: "#F7A84B",
+              ":hover": { backgroundColor: "#F7884B" },
+            }}
+            variant="contained"
+            onClick={handleSave}
+          >
+            Save
+          </Button>
+          <Button
+            sx={{
+              backgroundColor: "#F7A84B",
+              ":hover": { backgroundColor: "#F7884B" },
+            }}
+            variant="contained"
+            onClick={handleCancel}
+          >
+            Cancel
+          </Button>
+        </Stack>
+      </AccordionActions>
     </Accordion>
   );
 }
