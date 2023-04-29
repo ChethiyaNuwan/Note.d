59113da7314ae2d649ffcc775779479ca39684ae
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-04-30 02:48:02 +0530

    note interface, push note to notes[] on save

--- client/src/NewNote.tsx
+++ client/src/NewNote.tsx
@@ -6,82 +6,110 @@ import Typography from "@mui/material/Typography";
 import Input from "@mui/material/Input";
 import { AccordionActions, Button, Stack } from "@mui/material";
 
+interface Note {
+  id: number;
+  title: string;
+  note: string;
+}
+
 export default function NewNote() {
   const [isExpanded, setIsExpanded] = useState(false);
+  const notes: Note[] = [];
+  const [note, setNote] = useState("");
 
   const handleCancel = () => {
+    setNote("");
     setIsExpanded(false);
   };
 
-  const handleSave = () => {
+  const handleSave = (e: any) => {
+    e.preventDefault();
     setIsExpanded(false);
+    notes.push({
+      id: Math.floor(Math.random() * 1000),
+      title: e.target.title.value,
+      note: e.target.note.value,
+    });
+    setNote("");
+    console.log(notes);
   };
 
   return (
-    <Accordion
-      sx={{
-        width: "40%",
-        left: "30%",
-        right: "30%",
-        top: "20px",
-      }}
-      onChange={() => {
-        setIsExpanded(true);
-      }}
-      expanded={isExpanded}
-    >
-      <AccordionSummary
+    <form method="post" onSubmit={handleSave}>
+      <Accordion
         sx={{
-          margin: "0",
+          width: "40%",
+          left: "30%",
+          right: "30%",
+          top: "20px",
         }}
+        onChange={() => {
+          setIsExpanded(true);
+        }}
+        expanded={isExpanded}
       >
-        <Typography
+        <AccordionSummary
           sx={{
-            color: "gray",
-            width: "100%",
+            margin: "0",
           }}
         >
-          {!isExpanded ? (
-            "Add New Note"
-          ) : (
-            <Input fullWidth placeholder="Title" disableUnderline></Input>
-          )}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Input
-          fullWidth
-          placeholder="Add a note"
-          disableUnderline
-          multiline
-          minRows={10}
-          maxRows={10}
-        ></Input>
-      </AccordionDetails>
-      <AccordionActions>
-        <Stack spacing={1} direction="row">
-          <Button
+          <Typography
             sx={{
-              backgroundColor: "#F7A84B",
-              ":hover": { backgroundColor: "#F7884B" },
+              color: "gray",
+              width: "100%",
             }}
-            variant="contained"
-            onClick={handleSave}
           >
-            Save
-          </Button>
-          <Button
-            sx={{
-              backgroundColor: "#F7A84B",
-              ":hover": { backgroundColor: "#F7884B" },
-            }}
-            variant="contained"
-            onClick={handleCancel}
-          >
-            Cancel
-          </Button>
-        </Stack>
-      </AccordionActions>
-    </Accordion>
+            {!isExpanded ? (
+              "Add New Note"
+            ) : (
+              <Input
+                fullWidth
+                placeholder="Title"
+                disableUnderline
+                required
+                name="title"
+              ></Input>
+            )}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Input
+            fullWidth
+            placeholder="Add a note"
+            disableUnderline
+            multiline
+            minRows={10}
+            maxRows={10}
+            name="note"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          ></Input>
+        </AccordionDetails>
+        <AccordionActions>
+          <Stack spacing={1} direction="row">
+            <Button
+              sx={{
+                backgroundColor: "#F7A84B",
+                ":hover": { backgroundColor: "#F7884B" },
+              }}
+              variant="contained"
+              type="submit"
+            >
+              Save
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "#F7A84B",
+                ":hover": { backgroundColor: "#F7884B" },
+              }}
+              variant="contained"
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </Stack>
+        </AccordionActions>
+      </Accordion>
+    </form>
   );
 }
